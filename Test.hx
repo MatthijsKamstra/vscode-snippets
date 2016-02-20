42238f422f55342db62c6a25d15af62c5b9ff1f5
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2016-02-20 16:27:47 +0100

    fixed prop-snippet

--- Test.hx
+++ Test.hx
@@ -1,4 +1,17 @@
 class Test {
+	
+	
+	public var gogo ( get_gogo , set_gogo ) : String;
+	private var _gogo : String;
+	
+	function get_gogo () : String {
+		return _gogo;
+	}
+	function set_gogo(value : String) : String {
+		return _gogo = value;
+	}
+	
+	
     public function new () {
         // your code
     }
